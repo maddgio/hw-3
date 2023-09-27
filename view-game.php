d54864755c5aba5254a8d1ce63ad29290a8c39d3
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-09-27 14:50:52 -0500

    Create view-game.php

---
+++ view-game.php
@@ -0,0 +1,25 @@
+<h1>Games</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Date</th>
+        <th>Location</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($game = $game->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $game['game_id']; ?></td>
+  <td><?php echo $game['game_date']; ?></td>
+  <td><?php echo $game['game_location']; ?></td>
+</tr>
+<?php
+}
+?> 
+    </tbody>
+  </table>
+</div>
