8bead86788a39772c12ca1eec7fdbd8351f74c20
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-02 16:56:39 -0500

    Update view-team-stats.php

--- view-team-stats.php
+++ view-team-stats.php
@@ -3,11 +3,11 @@
   <table class="table">
     <thead>
       <tr>
-        <th>Team ID</th>
         <th>Game ID</th>
         <th>Game Date</th>
         <th>Game Location</th>
         <th>Player ID</th>
+        <th>Player Name</th>
         <th>Time Played</th>
         <th>Shots Made/Saved</th>
       </tr>
@@ -22,6 +22,7 @@ while ($team_stats = $Stats->fetch_assoc()) {
   <td><?php echo $team_stats['game_date']; ?></td>
   <td><?php echo $team_stats['game_location']; ?></td>
   <td><?php echo $team_stats['player_id']; ?></td>
+  <td><?php echo $team_stats['player_name']; ?></td>
   <td><?php echo $team_stats['time_played']; ?></td>
   <td><?php echo $team_stats['shots']; ?></td>
 </tr>
