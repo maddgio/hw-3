63918b3f52872759bdc62106ab4941a5c9a53fb9
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-02 15:42:42 -0500

    Update team-stats.php

--- team-stats.php
+++ team-stats.php
@@ -1,1 +1,10 @@
-..
+<?php
+require_once("util-db.php");
+require_once("model-team-stats.php");
+
+$pageTitle = "Team Stats";
+include "view-header.php";
+$Stats = selectStats();
+include "view-team-stats.php";
+include "view-footer.php";
+?>
