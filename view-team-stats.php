d524748588eead5265e4a7d806f71aa3d2428f9b
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-02 15:50:11 -0500

    Update view-team-stats.php

--- view-team-stats.php
+++ view-team-stats.php
@@ -1,1 +1,29 @@
-...
+<h1>Team Stats</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Team ID</th>
+        <th>Game ID</th>
+        <th>Player ID</th>
+        <th>Time Played</th>
+        <th>Shots Made/Saved</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($team_stats = $Stats->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $team_stats['team_id']; ?></td>
+  <td><?php echo $team_stats['game_id']; ?></td>
+  <td><?php echo $team_stats['player_id']; ?></td>
+  <td><?php echo $team_stats['time_played']; ?></td>
+  <td><?php echo $team_stats['shots']; ?></td>
+</tr>
+<?PHP
+}
+?> 
+    </tbody>
+  </table>
+</div>
