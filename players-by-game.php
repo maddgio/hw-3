b758bf1437b0c78a70f56d6bef44377f7fa436df
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-03 23:26:49 -0500

    Update players-by-game.php

--- players-by-game.php
+++ players-by-game.php
@@ -4,7 +4,7 @@ require_once("model-players-by-game.php");
 
 $pageTitle = "Players by Game";
 include "view-header.php";
-$GamePlayers = SelectGamePlayers($_POST['gpid']);
+$GamePlayers = SelectGamePlayers($_POST['pid']);
 include "view-players-by-game.php";
 include "view-footer.php";
 ?>
