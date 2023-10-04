2caee066c1587efe35ea613332fed6e275b87c54
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-03 23:23:25 -0500

    Update view-game.php

--- view-game.php
+++ view-game.php
@@ -7,6 +7,7 @@
         <th>Date</th>
         <th>Location</th>
         <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -14,10 +15,17 @@
 while ($lacrosse_game = $Game->fetch_assoc()) {
 ?>
 <tr>
-  <td><?php echo $lacrosse_game['game_id']; ?></td>
-  <td><?php echo $lacrosse_game['game_date']; ?></td>
-  <td><?php echo $lacrosse_game['game_location']; ?></td>
-  <td><a href="team-stats.php?id=<?php echo $lacrosse_game['game_id']; ?>">Team Stats</a></td>
+        <td><?php echo $lacrosse_game['game_id']; ?></td>
+        <td><?php echo $lacrosse_game['game_date']; ?></td>
+        <td><?php echo $lacrosse_game['game_location']; ?></td>
+  <td>
+      <form method="post" action="players-by-game.php">
+        <input tyoe-"hidden" name="gpid" value="<?php echo $lacrosse_game['game_id']; ?>">
+        <button type="submit" class="btn btn-primary">Players for Games</button>
+      </form>
+          
+  </td>
+    
 </tr>
 <?PHP
 }
