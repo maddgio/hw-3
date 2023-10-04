baf7419ba9f05ba0030831a3f372088c76c3cd31
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-04 00:07:58 -0500

    Update view-player-with-game.php

--- view-player-with-game.php
+++ view-player-with-game.php
@@ -7,13 +7,16 @@ while ($lax_player = $Player->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $lax_player['player_name']; ?></h5>
       <p class="card-text">
+          <ul class="list-group">
         <?PHP
           $games = SelectPlayersByGame($lax_player['player_id']; ?>);
           while ($games = $game->fetch_assoc()) {
         ?>
+            <li class="list-group-item"><?php echo $pgame['game_id']; ?> - <?php echo $pgame['game_date']; ?> - <?php echo $pgame['player_name']; ?> - <?php echo $pgame['player_name']; ?></li>
   <?php
 }
 ?>
+</ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Player; <?php echo $lax_player['player_id']; ?></small></p>
     </div>
