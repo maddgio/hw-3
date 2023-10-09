ecc430691e107a284d2ec86d3afbb9705587e7b7
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-09 14:50:45 -0500

    Update view-player.php

--- view-player.php
+++ view-player.php
@@ -19,7 +19,7 @@ while ($lax_player = $Player->fetch_assoc()) {
   <td><?php echo $lax_player['player_name']; ?></td>
   <td><?php echo $lax_player['player_position']; ?></td>
   <td><?php echo $lax_player['player_year']; ?></td>
-  <td><a href=http://hw3.giovenco.oucreate.com/player-with-games.php></td>
+  <td><a href="page.html">Player with Game</a></td>
 </tr>
 <?PHP
 }
