89285fb4d24829e741d6486180ced7ca25c9fb53
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-09 13:59:04 -0500

    Update model-team-stats.php

--- model-team-stats.php
+++ model-team-stats.php
@@ -3,7 +3,7 @@ function SelectStats() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT g.game_id, game_date, game_location, p.player_id, p.player_name, time_played, shots FROM `lacrosse_games` g join team t on t.game_id=g.game_id join player p on p.player_id=t.player_id");
-        $stmt->bind_param("i", $pid);
+        $stmt->bind_param("i");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
