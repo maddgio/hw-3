f566d7dea70138190f23fe5b2e03ae381c34daa3
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-11 14:18:03 -0500

    Update model-player-with-game.php
    taking out gid and execute line

--- model-player-with-game.php
+++ model-player-with-game.php
@@ -1,9 +1,8 @@
 <?php
-function SelectGamePlayers($gid) {
+function SelectGamePlayers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id WHERE t.player_id=?");
-        $stmt->bind_param("i", $gid);
+        $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
