f9671d4e663551cda27a203eed70e8756f2a38d5
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-11 14:14:45 -0500

    Update view-player-with-game.php

--- view-player-with-game.php
+++ view-player-with-game.php
@@ -1,26 +1,29 @@
-<h1>Players with Game</h1>
-<div class="card-group">
-<?php
-while ($lax_player = $Player->fetch_assoc()) {
+<h1>Players with Game </h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Game ID</th>
+        <th>Player ID</th>
+        <th>Player Name</th>
+      </tr>
+    </thead>
+    <tbody>
+<?PHP 
+while ($pgame = $GamePlayers->fetch_assoc()) {
 ?>
-    <div class="card">
-        <div class="card-body">
-              <h5 class="card-title"><?php echo $lax_player['player_name']; ?></h5>
-              <p class="card-text">
-          <ul class="list-group">
-        <?php
-          $Games = SelectGamePlayers($lax_player['player_id']);
-          while ($game = $GamePlayers->fetch_assoc()) {
-        ?>
-            <li class="list-group-item"><?php echo $game['game_id']; ?> - <?php echo $game['game_date']; ?> - <?php echo $game['player_name']; ?></li>
-  <?php
-}
-?>
-</ul>
-      <p class="card-text"><small class="text-body-secondary">Player: <?php echo $lax_player['player_id']; ?></small></p>
-    </div>
-<?php
+<tr>
+  <td><?php echo $pgame['game_id']; ?></td>
+ <td><?php echo $pgame['player_id']; ?></td>
+  <td><?php echo $pgame['player_name']; ?></td>
+</tr>
+<?PHP
 }
 ?> 
+    </tbody>
+  </table>
 </div>
+
+
+<tr>
 
