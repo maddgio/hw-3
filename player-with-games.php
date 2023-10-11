aa1eef1342be43eaa6fd226e7b9a9d13913ce541
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-11 14:17:17 -0500

    Update player-with-games.php

--- player-with-games.php
+++ player-with-games.php
@@ -4,7 +4,7 @@ require_once("model-player-with-game.php");
 
 $pageTitle = "Player with Game";
 include "view-header.php";
-$GamePlayers = SelectGamePlayers($_POST['gid']);
+$GamePlayers = SelectGamePlayers();
 include "view-player-with-game.php";
 include "view-footer.php";
 ?>
