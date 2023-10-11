dc1e6e46b831c654aeb35ce0715c4a70fab834e3
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-11 14:35:57 -0500

    Update model-player-with-game.php

--- model-player-with-game.php
+++ model-player-with-game.php
@@ -3,6 +3,7 @@ function SelectGamePlayers($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id where p.player_id=?");
+        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
