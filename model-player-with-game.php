680fad50d23d9851849e7b8acd0d4bf3c9b50715
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-11 11:10:16 -0500

    Update model-player-with-game.php

--- model-player-with-game.php
+++ model-player-with-game.php
@@ -1,26 +1,10 @@
-<?php
-function selectPlayers() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_id, player_name, player_position, player_year FROM `player` WHERE player_id=?");
-        $stmt->bind_param("i", $pid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-?> 
+
 
 <?php
-function SelectGamePlayers($pid) {
+function SelectPlayerWithGame() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT g.game_id, game_date, p.player_id, p.player_name FROM `lacrosse_games` g join team t on t.game_id=g.game_id join player p on p.player_id=t.player_id WHERE t.player_id=?");
-        $stmt->bind_param("i", $pid);
+        $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
