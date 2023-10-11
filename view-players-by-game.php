7772b76ccc0859647f16552638f417cd70d7c116
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-11 10:50:56 -0500

    Update view-players-by-game.php

--- view-players-by-game.php
+++ view-players-by-game.php
@@ -11,7 +11,7 @@
     </thead>
     <tbody>
 <?PHP 
-while ($pgame = $SelectGamePlayers->fetch_assoc()) {
+while ($pgame = $PlayersByGame->fetch_assoc()) {
 ?>
 <tr>
   <td><?php echo $pgame['game_id']; ?></td>
