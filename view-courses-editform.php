77e81cd99000ddc9e4981b19a139ecfdc6c12e0d
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 11:51:50 -0500

    Create view-courses-editform.php

---
+++ view-courses-editform.php
@@ -0,0 +1,24 @@
+<!-- Modal -->
+<div class="modal fade" id="newGameModal" tabindex="-1" aria-labelledby="newGameModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newGameModalLabel">New Game</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <div class="mb-3">
+            <label for="gDate" class="form-label">Game Date</label>
+          <input type="text" class="form-control" id="gDate" name="gDate">
+          </div>
+    <div class="mb-3">
+    <label for="gLocation" class="form-label">Game Location</label>
+    <input type="text" class="form-control" id="gLocation" name="gLocation">
+  </div>
+  <button type="submit" class="btn btn-primary">Save</button>
+</form>      
+      </div>
+    </div>
+  </div>
+</div>
