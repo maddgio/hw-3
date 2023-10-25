ef35b2ffe3719cac1fecc5a9a034e48c5aa82524
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-24 23:05:42 -0500

    Update model-game.php

--- model-game.php
+++ model-game.php
@@ -12,4 +12,46 @@ function selectGame() {
         throw $e;
     }
 }
+
+function inserttGame($gDate, $gLocation) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `lacrosse_games` (`game_date`, `game_location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $gDate, $gLocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateGame($gDate, $gLocation, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update 'lacrosse_games` set (`game_date`, `game_location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $gDate, $gLocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function inserttGame() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `lacrosse_games` (`game_date`, `game_location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $gDate, $gLocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
