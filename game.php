13196a4b2138c53dc71c6df2ca935fc9496a9e70
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 10:47:57 -0500

    Update game.php

--- game.php
+++ game.php
@@ -4,6 +4,14 @@ require_once("model-game.php");
 
 $pageTitle = "Game";
 include "view-header.php";
+
+if (isset(9$_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+    insertGame($_POST['gDate'], $_POST['gLocation']);
+    break;
+  }
+}
 $Game = selectGame();
 include "view-game.php";
 include "view-footer.php";
