b9d000a71a22b0ce37dcf343012aced078c3c97b
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 12:30:44 -0500

    Update view-games-newform.php

--- view-games-newform.php
+++ view-games-newform.php
@@ -27,8 +27,6 @@
     <input type="text" class="form-control" id="gLocation" name="gLocation">
   </div>
           <input type="hidden" name="actionType" value="Add">
-                    <input type="hidden" name="actionType" value="Delete">
-
   <button type="submit" class="btn btn-primary">Save</button>
 </form>      
       </div>
