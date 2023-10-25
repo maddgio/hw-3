cd8830721907e035f1f818f03c4a632168a8e632
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 12:03:33 -0500

    Update game.php

--- game.php
+++ game.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
 }
     break;
+     case "Edit":
+    if (updateGame($_POST['gDate'], $_POST['gLocation'], $_POST['gid'])) {
+    echo '<div class="alert alert-success" role="alert">Game Edited</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+}
+    break;
     case "Delete":
     if (deleteGame($_POST['gid'])) {
     echo '<div class="alert alert-success" role="alert">Game Deleted</div>';
