6c4f46f1fbcf3ce2f0c609f12a1441d14fd2df6d
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 11:02:49 -0500

    Update game.php

--- game.php
+++ game.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-    insertGame($_POST['gDate'], $_POST['gLocation']);
+    if (insertGame($_POST['gDate'], $_POST['gLocation']));
+    echo '<div class="alert alert-success" role="alert">Game Added</div>"';
+  } else {
+    echo '<div class="alert alert-success" role="alert">Error</div>"';
+}
     break;
   }
 }
