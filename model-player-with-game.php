d342d90c534fce02e03e5418ae42e377c458b7a5
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 12:29:01 -0500

    MIGHT DELETE

--- model-player-with-game.php
+++ model-player-with-game.php
@@ -13,5 +13,51 @@ function SelectGamePlayers($pid) {
         throw $e;
     }
 }
+
+function insertGamePlayers($pid, $gid, $pname) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id where p.player_id=?");
+        $stmt->bind_param("i", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateGamePlayers($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id where p.player_id=?");
+        $stmt->bind_param("i", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteGamePlayers($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id where p.player_id=?");
+        $stmt->bind_param("i", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
 
