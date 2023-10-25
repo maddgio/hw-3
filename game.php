b64caf099320f397cc7dcab0880e1c9d86e2c92a
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 11:15:43 -0500

    Update game.php

--- game.php
+++ game.php
@@ -9,9 +9,9 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
     if (insertGame($_POST['gDate'], $_POST['gLocation'])) {
-    echo '<div class="alert alert-success" role="alert">Game Added</div>"';
+    echo '<div class="alert alert-success" role="alert">Game Added</div>';
   } else {
-    echo '<div class="alert alert-success" role="alert">Error</div>"';
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
 }
     break;
   }
