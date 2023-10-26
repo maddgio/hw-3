fb32e7696eba8611c3619f494eb39a0b0957840f
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 20:00:09 -0500

    Update game.php

--- game.php
+++ game.php
@@ -1,10 +1,37 @@
 <?php
 require_once("util-db.php");
 require_once("model-game.php");
-
-$pageTitle = "Game";
+  
+$pageTitle = "Games";
 include "view-header.php";
-$Game = selectGame();
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertGame($_POST['gDate'], $_POST['gLocation'])) {
+                echo '<div class="alert alert-success" role="alert">Game Added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+        case "Edit":
+            if (updateGame($_POST['gDate'], $_POST['gLocation'], $_POST['gid'])) {
+                echo '<div class="alert alert-success" role="alert">Game Edited.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+        case "Delete":
+            if (deleteGame($_POST['gid'])) {
+                echo '<div class="alert alert-success" role="alert">Game Deleted.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+    }
+}
+
+$buses = selectGame();
 include "view-game.php";
 include "view-footer.php";
 ?>
