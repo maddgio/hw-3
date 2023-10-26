48ebf5df7a1db22c329913b5552bbfdaa47e0ed7
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 23:50:44 -0500

    Update game.php

--- game.php
+++ game.php
@@ -1,37 +1,34 @@
 <?php
 require_once("util-db.php");
 require_once("model-game.php");
-  
 $pageTitle = "Games";
 include "view-header.php";
-
 if (isset($_POST['actionType'])) {
-    switch ($_POST['actionType']) {
-        case "Add":
-            if (insertGame($_POST['gDate'], $_POST['gLocation'])) {
-                echo '<div class="alert alert-success" role="alert">Game Added.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error.</div>';
-            }
-            break;
-        case "Edit":
-            if (updateGame($_POST['gDate'], $_POST['gLocation'], $_POST['gid'])) {
-                echo '<div class="alert alert-success" role="alert">Game Edited.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error.</div>';
-            }
-            break;
-        case "Delete":
-            if (deleteGame($_POST['gid'])) {
-                echo '<div class="alert alert-success" role="alert">Game Deleted.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error.</div>';
-            }
-            break;
-    }
+  switch ($_POST['actionType']) {
+    case "Add":
+    if (insertGame($_POST['gDate'], $_POST['gLocation'])) {
+    echo '<div class="alert alert-success" role="alert">Game Added</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
 }
-
-$buses = selectGame();
+    break;
+     case "Edit":
+    if (updateGame($_POST['gDate'], $_POST['gLocation'], $_POST['gid'])) {
+    echo '<div class="alert alert-success" role="alert">Game Edited</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+}
+    break;
+    case "Delete":
+    if (deleteGame($_POST['gid'])) {
+    echo '<div class="alert alert-success" role="alert">Game Deleted</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+}
+    break;
+  }
+}
+$Game = selectGame();
 include "view-game.php";
 include "view-footer.php";
 ?>
