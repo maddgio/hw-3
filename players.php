fa29d632520de2c6c95284aa5c829cc3ec52784c
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-26 00:14:20 -0500

    check to see if this adds the buttons

--- players.php
+++ players.php
@@ -1,37 +1,36 @@
+
+
 <?php
 require_once("util-db.php");
 require_once("model-player.php");
-  
 $pageTitle = "Players";
 include "view-header.php";
-
 if (isset($_POST['actionType'])) {
-    switch ($_POST['actionType']) {
-        case "Add":
-            if (insertPlayers($_POST['pName'], $_POST['pPosition'])) {
-                echo '<div class="alert alert-success" role="alert">Player Added</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error.</div>';
-            }
-            break;
-        case "Edit":
-            if (updatePlayers($_POST['pName'], $_POST['pPosition'], $_POST['pid'])) {
-                echo '<div class="alert alert-success" role="alert">Player Edited.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error.</div>';
-            }
-            break;
-        case "Delete":
-            if (deletePlayers($_POST['pid'])) {
-                echo '<div class="alert alert-success" role="alert">Player Deleted.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error.</div>';
-            }
-            break;
-    }
+  switch ($_POST['actionType']) {
+    case "Add":
+    if (insertPlayer($_POST['pName'], $_POST['pPosition'])) {
+    echo '<div class="alert alert-success" role="alert">Player Added</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
 }
-
-$Player = selectPlayers();
+    break;
+     case "Edit":
+    if (updatePlayer($_POST['pName'], $_POST['pPosition'], $_POST['pid'])) {
+    echo '<div class="alert alert-success" role="alert">Game Edited</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+}
+    break;
+    case "Delete":
+    if (deletePlayer($_POST['pid'])) {
+    echo '<div class="alert alert-success" role="alert">Player Deleted</div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error</div>';
+}
+    break;
+  }
+}
+$Game = selectPlayers();
 include "view-player.php";
 include "view-footer.php";
 ?>
