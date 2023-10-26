2455bc4dc90d0d77bae4fff349e9773aeaff84c6
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 20:10:48 -0500

    Update model-player-with-game.php

--- model-player-with-game.php
+++ model-player-with-game.php
@@ -1,8 +1,22 @@
 <?php
-function SelectGamePlayers($pid) {
+function selectPlayer() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT t.player_id, p.player_name, t.game_id FROM `team` t join player p on t.player_id=p.player_id where p.player_id=?");
+        $stmt = $conn->prepare("SELECT player_id, player_name, player_position, player_year FROM `player`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectPlayerbyGame($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT t.game_id, p.player_id, t.team_id, player_name, t.time_played FROM `player` p join Team t on p.player_id = t.player_id where t.player_id=?");
         $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -13,5 +27,32 @@ function SelectGamePlayers($pid) {
         throw $e;
     }
 }
-?>
+
+function selectPlayersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_id, player_name FROM `player` order by player_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectGamesForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT game_id, game_date FROM `lacrosse_games` order by game_date");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
