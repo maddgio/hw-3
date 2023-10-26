2018eed7ebfb3edea9a56ac985300e0a955b1097
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-26 00:24:50 -0500

    Update players.php

--- players.php
+++ players.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-    if (insertPlayer($_POST['pName'], $_POST['pPosition'])) {
+    if (insertPlayers($_POST['pName'], $_POST['pPosition'])) {
     echo '<div class="alert alert-success" role="alert">Player Added</div>';
   } else {
     echo '<div class="alert alert-danger" role="alert">Error</div>';
