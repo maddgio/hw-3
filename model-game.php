ab30550acfab2f8abf58336144c7a8cae94b646f
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-10-25 20:03:36 -0500

    Update model-game.php

--- model-game.php
+++ model-game.php
@@ -2,11 +2,53 @@
 function selectGame() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT game_id, game_date, game_location FROM `lacrosse_games` ");
+        $stmt = $conn->prepare("SELECT game_id, game_date, game_location FROM `lacrosse_games`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertGame($gDate, $gLocation) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `lacrosse_games` (`game_date`, `game_location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $gDate, $gLocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateGame($gDate, $gLocation, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `lacrosse_games` SET `game_date` = ?, `game_location` = ? WHERE `lacrosse_games`.`game_id` = ?");
+        $stmt->bind_param("ssi", $gDate, $gLocation, $gid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteGame($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `lacrosse_games` where game_id = ?");
+        $stmt->bind_param("i", $gid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
