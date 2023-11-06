8cb72aef70d2044f58647a211af1c8fec0b9045b
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-11-05 21:33:01 -0600

    Update game.php

--- game.php
+++ game.php
@@ -4,6 +4,33 @@ require_once("model-game.php");
 
 $pageTitle = "Game";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertGame($_POST['gDate'], $_POST['gLocation'])) {
+                echo '<div class="alert alert-success" role="alert">Game Added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+        case "Edit":
+            if (updateGame($_POST['gDate'], $_POST['gLocation'], $_POST['gid'])) {
+                echo '<div class="alert alert-success" role="alert">Game Edited.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+        case "Delete":
+            if (deleteGame($_POST['gid'])) {
+                echo '<div class="alert alert-success" role="alert">Game Deleted.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+    }
+}
+
 $Game = selectGame();
 include "view-game.php";
 include "view-footer.php";
